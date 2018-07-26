ec5f00bbbaaf7d499e2f1c0b76bfb2c76074d904
Ian Drysdale <idrysdale@gmail.com>
2018-07-26 10:40:18 +0100

    Change URL

--- supernode/app.py
+++ supernode/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/snapshot')
+@app.route('/save-time-syncing-by-downloading-blockchain/')
 def snapshot():
     return render_template('snapshot.html')
 
