d8b2fc94aee70c38640c822e05a44d3f50d0ac7e
Paul M Furley <paul@paulfurley.com>
2018-07-31 15:30:57 +0100

    Add model, run `flask db init`

--- supernode/app.py
+++ supernode/app.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python
 
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class Invoice(db.Model):
+    payment_request = db.Column(db.String(500), primary_key=True)
+    paid = db.Column(db.Boolean)
 
 
 @app.route("/")
